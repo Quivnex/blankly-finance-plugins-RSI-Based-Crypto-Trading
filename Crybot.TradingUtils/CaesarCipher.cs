65584078d8538440fa34513aadb9eec776253562
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:48 +0300

    CaesarCipher.cs

---
+++ Crybot.TradingUtils/CaesarCipher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substitution
+{
+    internal class CaesarCipher : ShiftByNCipher
+    {
+        public CaesarCipher(): base(3)
+        {
+
+        }
+    }
+}
