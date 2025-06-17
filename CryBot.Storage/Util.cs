c1bc97d789eb1f86767d67c1d811d4a56b438d3e
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:41 +0300

    Util.cs

---
+++ CryBot.Storage/Util.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using System;
+
+namespace Substitution
+{
+    internal class Util
+    {
+        public static void Shuffle(StringBuilder sb)
+        {
+            Random rnd = new Random();
+            int j;
+            for (int i = 25; i > 0; i--)
+            {
+                j = rnd.Next(i + 1);
+                (sb[i], sb[j]) = (sb[j], sb[i]);
+            }
+        }
+    }
+}
